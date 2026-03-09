772efcea36c0d34337fbbb18b28db85eac996d6f
leo-grip <leojung@gripcorp.co>
2026-03-09 10:57:20 +0900

    chore: 프로젝트 빌드

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.12-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "grip.show"
+version = "0.0.1-SNAPSHOT"
+description = "app"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
